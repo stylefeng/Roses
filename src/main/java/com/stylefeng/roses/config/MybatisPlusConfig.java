f58d6f1d268dea9de948cd4af3a9785743f399a4
fsn <sn93@qq.com>
2017-05-29 21:47:31 +0800

    初始化mp并测试插入数据成功

---
+++ src/main/java/com/stylefeng/roses/config/MybatisPlusConfig.java
@@ -0,0 +1,44 @@
+package com.stylefeng.roses.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.baomidou.mybatisplus.enums.DBType;
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import com.stylefeng.roses.config.properties.DruidProperties;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MybatisPlus配置
+ *
+ * @author stylefeng
+ * @Date 2017/5/20 21:58
+ */
+@Configuration
+@MapperScan(basePackages = {"com.stylefeng.roses.modular.*.dao", "com.stylefeng.roses.persistence.dao"})
+public class MybatisPlusConfig {
+
+    @Autowired
+    DruidProperties druidProperties;
+
+    /**
+     * mybatis-plus分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        paginationInterceptor.setDialectType(DBType.MYSQL.getDb());
+        return paginationInterceptor;
+    }
+
+    /**
+     * druid数据库连接池
+     */
+    @Bean(initMethod = "init")
+    public DruidDataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        druidProperties.coinfig(dataSource);
+        return dataSource;
+    }
+}
