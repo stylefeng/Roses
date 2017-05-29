86fc0158eb28716837db84b4f17d98abb27c1577
fsn <sn93@qq.com>
2017-05-29 22:58:07 +0800

    重新规划项目结构

---
+++ roses-message-service-facade/src/main/java/com/stylefeng/roses/facade/MessageServiceApi.java
@@ -0,0 +1,13 @@
+package com.stylefeng.roses.facade;
+
+/**
+ * 消息服务子系统开放的接口
+ *
+ * @author fengshuonan
+ * @date 2017-05-29 22:43
+ */
+public interface MessageServiceApi {
+
+
+
+}
---
+++ roses-message-service/src/main/java/com/stylefeng/roses/service/MessageServiceApiImpl.java
@@ -0,0 +1,12 @@
+package com.stylefeng.roses.service;
+
+import com.stylefeng.roses.facade.MessageServiceApi;
+
+/**
+ * 消息服务子系统接口的实现
+ *
+ * @author fengshuonan
+ * @date 2017-05-29 22:44
+ */
+public class MessageServiceApiImpl implements MessageServiceApi {
+}
