8ad13c5b0f046f2e42f155b6d9feedfd5bfda24a
naan1993 <sn93@qq.com>
2017-06-18 23:22:56 +0800

    升级最新springboot，把eureka加入到base子模块

--- roses-eureka-server/src/main/java/com/stylefeng/roses/eureka/RosesEurekaServerApplication.java
+++ roses-eureka-server/src/main/java/com/stylefeng/roses/eureka/RosesEurekaServerApplication.java
@@ -1,5 +1,6 @@
 package com.stylefeng.roses.eureka;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
