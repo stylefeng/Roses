64f2f4acca71270f6068aceafe6d81f7f653cb74
naan1993 <sn93@qq.com>
2017-06-18 23:04:39 +0800

    初始化eureka服务中心

---
+++ roses-eureka-server/src/main/java/com/stylefeng/roses/eureka/RosesEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.stylefeng.roses.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RosesEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosesEurekaServerApplication.class, args);
+	}
+}
