51c22cd681ca2b7b271045420e11e18e4662ae8d
naan1993 <sn93@qq.com>
2017-07-06 23:19:00 +0800

    完善注释

--- roses-eureka-server/src/main/java/com/stylefeng/roses/eureka/RosesEurekaServerApplication.java
+++ roses-eureka-server/src/main/java/com/stylefeng/roses/eureka/RosesEurekaServerApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Eureka服务治理主类
+ *
+ * @author stylefeng
+ * @Date 2017/7/6 23:18
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class RosesEurekaServerApplication {
