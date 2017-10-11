50ca6eb5f30c4df3898d5553a1d434bf2ffd8ffc
naan1993 <sn93@qq.com>
2017-10-11 21:32:02 +0800

    增加积分模块

---
+++ roses-integral/src/main/java/com/styelfeng/roses/RosesIntegralApplication.java
@@ -0,0 +1,12 @@
+package com.styelfeng.roses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosesIntegralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosesOrderApplication.class, args);
+	}
+}
