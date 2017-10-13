ef3bb2f23e5539d69c3b2bc6a9a6f5c0f08822c4
naan1993 <sn93@qq.com>
2017-10-13 20:37:20 +0800

    完善积分和订单系统的宝扫描配置和pom依赖

--- roses-integral/src/main/java/com/styelfeng/roses/integral/RosesIntegralApplication.java
+++ roses-integral/src/main/java/com/styelfeng/roses/integral/RosesIntegralApplication.java
@@ -3,7 +3,7 @@ package com.styelfeng.roses.integral;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.stylefeng.roses")
 public class RosesIntegralApplication {
 
 	public static void main(String[] args) {
--- roses-order/src/main/java/com/styelfeng/roses/order/RosesOrderApplication.java
+++ roses-order/src/main/java/com/styelfeng/roses/order/RosesOrderApplication.java
@@ -3,10 +3,10 @@ package com.styelfeng.roses.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.stylefeng.roses")
 public class RosesOrderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RosesOrderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RosesOrderApplication.class, args);
+    }
 }
