79605c913666b1355ef514996a7abe943ac3cdbd
stylefeng <sn93@qq.com>
2017-10-22 21:18:17 +0800

    完善core

--- roses-core/src/main/java/com/stylefeng/roses/core/datasource/DruidProperties.java
+++ roses-core/src/main/java/com/stylefeng/roses/core/datasource/DruidProperties.java
@@ -1,4 +1,4 @@
-package com.stylefeng.roses.order.config.properties;
+package com.stylefeng.roses.core.datasource;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
