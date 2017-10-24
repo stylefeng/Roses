08ad6bb510fc1f4daa7b0357b8caaac8023033b4
naan1993 <sn93@qq.com>
2017-10-24 22:55:26 +0800

    删掉feign-consumer

--- roses-feign-consumer/src/main/java/com/stylefeng/roses/consumer/service/HelloService.java
+++
@@ -1,8 +0,0 @@
-package com.stylefeng.roses.consumer.service;
-
-import org.springframework.cloud.netflix.feign.FeignClient;
-
-@FeignClient("message-service")
-public interface HelloService extends com.stylefeng.roses.facade.hello.HelloService {
-
-}
