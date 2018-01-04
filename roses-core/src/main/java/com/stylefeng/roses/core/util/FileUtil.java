a8784dbf3946e38b0ef3abdf4dd0d4abcacc57cf
naan1993 <sn93@qq.com>
2018-01-04 23:09:53 +0800

    修改注释

--- roses-core/src/main/java/com/stylefeng/roses/core/util/FileUtil.java
+++ roses-core/src/main/java/com/stylefeng/roses/core/util/FileUtil.java
@@ -11,44 +11,47 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
+/**
+ * NIO方式读取文件
+ *
+ * @author stylefeng
+ * @Date 2018/1/4 23:09
+ */
 public class FileUtil {
-	
-	private static Logger log = LoggerFactory.getLogger(FileUtil.class);
-	
-	/**
-	 * NIO way
-	 */
-	public static byte[] toByteArray(String filename) {
 
-		File f = new File(filename);
-		if (!f.exists()) {
-			log.error("文件未找到！" + filename);
-			throw new ServiceException(CoreExceptionEnum.FILE_NOT_FOUND);
-		}
-		FileChannel channel = null;
-		FileInputStream fs = null;
-		try {
-			fs = new FileInputStream(f);
-			channel = fs.getChannel();
-			ByteBuffer byteBuffer = ByteBuffer.allocate((int) channel.size());
-			while ((channel.read(byteBuffer)) > 0) {
-				// do nothing
-				// System.out.println("reading");
-			}
-			return byteBuffer.array();
-		} catch (IOException e) {
-			throw new ServiceException(CoreExceptionEnum.FILE_READING_ERROR);
-		} finally {
-			try {
-				channel.close();
-			} catch (IOException e) {
-				throw new ServiceException(CoreExceptionEnum.FILE_READING_ERROR);
-			}
-			try {
-				fs.close();
-			} catch (IOException e) {
-				throw new ServiceException(CoreExceptionEnum.FILE_READING_ERROR);
-			}
-		}
-	}
+    private static Logger log = LoggerFactory.getLogger(FileUtil.class);
+
+    public static byte[] toByteArray(String filename) {
+
+        File f = new File(filename);
+        if (!f.exists()) {
+            log.error("文件未找到！" + filename);
+            throw new ServiceException(CoreExceptionEnum.FILE_NOT_FOUND);
+        }
+        FileChannel channel = null;
+        FileInputStream fs = null;
+        try {
+            fs = new FileInputStream(f);
+            channel = fs.getChannel();
+            ByteBuffer byteBuffer = ByteBuffer.allocate((int) channel.size());
+            while ((channel.read(byteBuffer)) > 0) {
+                // do nothing
+                // System.out.println("reading");
+            }
+            return byteBuffer.array();
+        } catch (IOException e) {
+            throw new ServiceException(CoreExceptionEnum.FILE_READING_ERROR);
+        } finally {
+            try {
+                channel.close();
+            } catch (IOException e) {
+                throw new ServiceException(CoreExceptionEnum.FILE_READING_ERROR);
+            }
+            try {
+                fs.close();
+            } catch (IOException e) {
+                throw new ServiceException(CoreExceptionEnum.FILE_READING_ERROR);
+            }
+        }
+    }
 }
