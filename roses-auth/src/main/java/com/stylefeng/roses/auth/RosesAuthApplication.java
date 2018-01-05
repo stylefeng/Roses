64e9f58b1e6303a91c103b147694d78365302583
stylefeng <sn93@qq.com>
2018-01-05 18:05:55 +0800

    增加auth模块

---
+++ roses-auth/src/main/java/com/stylefeng/roses/auth/RosesAuthApplication.java
@@ -0,0 +1,12 @@
+package com.stylefeng.roses.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosesAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosesAuthApplication.class, args);
+	}
+}
