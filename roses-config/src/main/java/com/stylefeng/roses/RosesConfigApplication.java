34dd95bb77e5db4d178aa9b64b3a407b763b05e4
naan1993 <sn93@qq.com>
2018-01-17 23:19:32 +0800

    配置中心增加eureka连接

--- roses-config/src/main/java/com/stylefeng/roses/RosesConfigApplication.java
+++ roses-config/src/main/java/com/stylefeng/roses/RosesConfigApplication.java
@@ -2,13 +2,30 @@ package com.stylefeng.roses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 配置中心启动类
+ *
+ * <pre>
+ * 获取git上的配置信息的访问地址:
+ *      /{application}/{profile}/{label}
+ *      /{application}-{profile}.yml
+ *      /{label}/{application}-{profile}.yml
+ *      /{application}-{profile}.properties
+ *      /{label}/{application}-{profile}.properties
+ * </pre>
+ *
+ * @author stylefeng
+ * @Date 2018/1/17 23:00
+ */
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class RosesConfigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RosesConfigApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RosesConfigApplication.class, args);
+    }
 }
