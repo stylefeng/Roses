0846a481adbf85172c255dcf040393ddf03600ee
stylefeng <sn93@qq.com>
2018-05-12 23:43:22 +0800

    配置签名过滤器

--- roses-gateway/src/main/java/com/stylefeng/roses/gate/config/FilterConfig.java
+++ roses-gateway/src/main/java/com/stylefeng/roses/gate/config/FilterConfig.java
@@ -1,9 +1,6 @@
 package com.stylefeng.roses.gate.config;
 
-import com.stylefeng.roses.gate.filter.DevelopFilter;
-import com.stylefeng.roses.gate.filter.JwtTokenFilter;
-import com.stylefeng.roses.gate.filter.PathMatchFilter;
-import com.stylefeng.roses.gate.filter.RequestNoGenerateFilter;
+import com.stylefeng.roses.gate.filter.*;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,24 +14,45 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FilterConfig {
 
-    @Bean
-    @ConditionalOnProperty(prefix = "develop", name = "open", havingValue = "false", matchIfMissing = true)
-    public JwtTokenFilter authFilter() {
-        return new JwtTokenFilter();
-    }
-
-    @Bean
-    @ConditionalOnProperty(prefix = "develop", name = "open", havingValue = "false", matchIfMissing = true)
-    public PathMatchFilter pathMatchFilter() {
-        return new PathMatchFilter();
-    }
-
+    /**
+     * 开发过滤器（免去每次都必须传用户id）
+     */
     @Bean
     @ConditionalOnProperty(prefix = "develop", name = "open", havingValue = "true")
     public DevelopFilter developFilter() {
         return new DevelopFilter();
     }
 
+    /**
+     * token过滤器，检查每次请求token是否合法
+     */
+    @Bean
+    @ConditionalOnProperty(prefix = "develop", name = "open", havingValue = "false", matchIfMissing = true)
+    public JwtTokenFilter authFilter() {
+        return new JwtTokenFilter();
+    }
+
+    /**
+     * 资源过滤器，检查每次请求是否有权限访问某些资源
+     */
+    @Bean
+    @ConditionalOnProperty(prefix = "develop", name = "open", havingValue = "false", matchIfMissing = true)
+    public PathMatchFilter pathMatchFilter() {
+        return new PathMatchFilter();
+    }
+
+    /**
+     * 签名过滤器，校验每次请求数据的内容是否签名合法
+     */
+    @Bean
+    @ConditionalOnProperty(prefix = "develop", name = "open", havingValue = "false", matchIfMissing = true)
+    public SignValidateFilter signValidateFilter() {
+        return new SignValidateFilter();
+    }
+
+    /**
+     * 请求唯一编号生成器，每次请求入网关时都会生成一个唯一编号，用来记录一次请求的所有日志和异常信息
+     */
     @Bean
     public RequestNoGenerateFilter requestNoGenerateFilter() {
         return new RequestNoGenerateFilter();
