187b29fbc25b7f1b7c01cf1e0494be07f919a264
stylefeng <sn93@qq.com>
2018-06-12 22:39:01 +0800

    完善分布式配置中心

--- roses-config/src/main/java/com/stylefeng/roses/RosesConfigApplication.java
+++ roses-config/src/main/java/com/stylefeng/roses/RosesConfigApplication.java
@@ -2,7 +2,6 @@ package com.stylefeng.roses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -22,7 +21,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
-@EnableDiscoveryClient
 public class RosesConfigApplication {
 
     public static void main(String[] args) {
