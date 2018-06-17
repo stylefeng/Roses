98cd3eea3fafcb743c34580840726ec12a3ea53f
stylefeng <sn93@qq.com>
2018-06-17 11:42:33 +0800

    完善监控中心和注册中心的配置

--- roses-monitor/src/main/java/com/stylefeng/roses/monitor/RosesMonitorApplication.java
+++ roses-monitor/src/main/java/com/stylefeng/roses/monitor/RosesMonitorApplication.java
@@ -2,19 +2,44 @@ package com.stylefeng.roses.monitor;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-/**
- * 监控服务
- *
- * @author stylefeng
- * @Date 2018/1/22 21:26
- */
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
 @EnableAdminServer
 public class RosesMonitorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RosesMonitorApplication.class, args);
     }
+
+    @Configuration
+    public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // Page with login form is served as /login.html and does a POST on /login
+            http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").permitAll();
+            // The UI does a POST on /logout on logout
+            http.logout().logoutUrl("/logout");
+            // The ui currently doesn't support csrf
+            http.csrf().disable();
+
+            // Requests for the login page and the static assets are allowed
+            http.authorizeRequests()
+                    .antMatchers("/login.html", "/**/*.css", "/img/**", "/third-party/**")
+                    .permitAll();
+            // ... and any other request needs to be authorized
+            http.authorizeRequests().antMatchers("/**").authenticated();
+
+            // Enable so that the clients can authenticate via HTTP basic for registering
+            http.httpBasic();
+        }
+    }
 }
+
