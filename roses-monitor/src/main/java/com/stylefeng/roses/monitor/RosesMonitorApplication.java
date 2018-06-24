a50cbb1741206433a734ab028629cd0d03c873d9
stylefeng <sn93@qq.com>
2018-06-24 23:02:10 +0800

    更新监控中心

--- roses-monitor/src/main/java/com/stylefeng/roses/monitor/RosesMonitorApplication.java
+++ roses-monitor/src/main/java/com/stylefeng/roses/monitor/RosesMonitorApplication.java
@@ -1,11 +1,21 @@
 package com.stylefeng.roses.monitor;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
+/**
+ * 监控中心启动类
+ *
+ * @author stylefeng
+ * @Date 2018/6/24 22:28
+ */
 @Configuration
 @EnableAutoConfiguration
 @EnableDiscoveryClient
@@ -16,5 +26,30 @@ public class RosesMonitorApplication {
         SpringApplication.run(RosesMonitorApplication.class, args);
     }
 
+    @Configuration
+    public static class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+        private final String adminContextPath;
+
+        public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+            this.adminContextPath = adminServerProperties.getContextPath();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            // @formatter:off
+            SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+            successHandler.setTargetUrlParameter("redirectTo");
+
+            http.authorizeRequests()
+                    .antMatchers(adminContextPath + "/assets/**").permitAll()
+                    .antMatchers(adminContextPath + "/login").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+                    .logout().logoutUrl(adminContextPath + "/logout").and()
+                    .httpBasic().and()
+                    .csrf().disable();
+        }
+    }
 }
 
