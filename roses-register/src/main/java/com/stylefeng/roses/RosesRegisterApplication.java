b3a8cd91476df327b1ec20a8cd8c626e83138901
stylefeng <sn93@qq.com>
2018-06-26 21:03:32 +0800

    完善配置

--- roses-register/src/main/java/com/stylefeng/roses/RosesRegisterApplication.java
+++ roses-register/src/main/java/com/stylefeng/roses/RosesRegisterApplication.java
@@ -3,6 +3,9 @@ package com.stylefeng.roses;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * 注册中心服务
@@ -16,5 +19,15 @@ public class RosesRegisterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RosesRegisterApplication.class, args);
+    }
+
+    @Configuration
+    public class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()
+                    .and().csrf().disable();
+        }
     }
 }
